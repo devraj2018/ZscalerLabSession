e2f6ae14ea3a825320701e4f10e2d9c1c1307342
Sidharth Rajagopal <62765220+professorbruh@users.noreply.github.com>
2022-04-27 15:44:40 +0530

    Added Transactions model

---
+++ src/main/java/com/zscalerlabsession/Model/Transactions.java
@@ -0,0 +1,81 @@
+package com.zscalerlabsession.Model;
+import javax.persistence.*;
+import java.sql.Date;
+
+public class Transactions {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String sender;
+    private String receiver;
+    private int amount;
+    private String status;
+    private Date date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Transactions() {
+        super();
+    }
+
+    public Transactions(long id, String sender, String receiver, int amount, String status, Date date) {
+        this.id = id;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.amount = amount;
+        this.status = status;
+        this.date = date;
+    }
+
+
+
+
+}
