8ad590ec55910a08019c5353d5d6f6e64cbc6e1f
devraj2018 <shekhawatdevraj123@gmail.com>
2022-04-28 14:43:46 +0530

    DS_Store

--- src/main/java/com/zscalerlabsession/controllers/CreateUserandLogin.java
+++ src/main/java/com/zscalerlabsession/controllers/CreateUserandLogin.java
@@ -28,4 +28,24 @@ public class CreateUserandLogin
         return "Testing";
     }
 
+    @PostMapping("/createCustomer")
+	public ResponseEntity<Object> createCustomer(@RequestBody CustomerDetails customerDetails){
+		
+		CustomerDetails fetchAdmin = authSerivce.fetchAdminByEmail(customerDetails.getEmailId());
+		
+		if(fetchCustomer == null) {
+			CustomerDetails admin = authSerivce.createCustomer(customerDetails);
+			
+			CustomerDetailsResponse response = new CustomerDetailsResponse(new Date(),"Customer Created Succesfully","200",admin);
+			
+			return new ResponseEntity<Object>(response,HttpStatus.OK);
+		}else {
+			
+			CustomResponseForNoUser response = new CustomResponseForNoUser(new Date(),"Email id already exists","409");
+			return new ResponseEntity<Object>(response,HttpStatus.OK);
+			
+		}	
+		
+	}
+
 }
